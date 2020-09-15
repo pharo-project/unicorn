df539fab8881a86c373b7906b0fda8b7c7e77d45
Nguyen Anh Quynh <aquynh@gmail.com>
2020-09-15 15:29:25 +0800

    add a test from #1316 to sample_x86.c

--- samples/sample_x86.c
+++ samples/sample_x86.c
@@ -1026,8 +1026,85 @@ static void test_x86_16(void)
     uc_close(uc);
 }
 
+// addr:
+//      INT 21h;
+//      jmp addr;
+static const char *cmd = "\xcd\x21\xeb\xfc";
+static int count = 1;
+
+static void cb(uc_engine *uc, uint32_t intno, void *user_data)
+{
+    printf("Callback count: %d\n", count);
+    count += 1;
+
+    if (count == 100)
+        uc_emu_stop(uc);
+}
+
+static int test_x16()
+{
+    uc_engine* uc;
+    uc_err err;
+
+    printf("Start\n");
+    err = uc_open(UC_ARCH_X86, UC_MODE_16, &uc);
+    if (err != UC_ERR_OK) {
+        printf("Failed to open uc\n");
+        return -1;
+    }
+
+    err = uc_mem_map(uc, 0x7000, 4 * 1024, UC_PROT_ALL);
+    if (err != UC_ERR_OK) {
+        printf("Failed to allocate memory with %d\n", err);
+        return -1;
+    }
+
+    int ip = 0x100;
+    int cs = 0x75a;
+    int address_to_load = cs * 16 + ip;
+    err = uc_mem_write(uc, address_to_load, cmd, 4);
+    if (err != UC_ERR_OK) {
+        printf("Failed to write memory with %d\n", err);
+        return -1;
+    }
+
+    err = uc_reg_write(uc, UC_X86_REG_IP, &ip);
+    if (err != UC_ERR_OK) {
+        printf("Failed to write register with %d\n", err);
+        return -1;
+    }
+
+    err = uc_reg_write(uc, UC_X86_REG_CS, &cs);
+    if (err != UC_ERR_OK) {
+        printf("Failed to write register with %d\n", err);
+        return -1;
+    }
+
+    uc_hook hook;
+    err = uc_hook_add(uc, &hook, UC_HOOK_INTR, (void*)cb, NULL, 0, -1);
+    if (err != UC_ERR_OK) {
+        printf("Hook failed with %d\n", err);
+        return -1;
+    }
+
+    printf("Before emulation.\n");
+    err = uc_emu_start(uc, address_to_load, address_to_load + 4, 0, 0);
+    if (err != UC_ERR_OK) {
+        printf("Emulation error %d\n", err);
+        return -1;
+    }
+
+    printf("After emulation.\n");
+    uc_close(uc);
+
+    return 0;
+}
+
 int main(int argc, char **argv, char **envp)
 {
+    test_x16();
+    return 0;
+
     if (argc == 2) {
         if (!strcmp(argv[1], "-16")) {
             test_x86_16();
